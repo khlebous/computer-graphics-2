721ca71030d7f7736ecad44a41787b9937f32e4d
khlebous <khlebousa@gmail.com>
2019-06-04 17:09:22 +0200

    calculate reflect and refract colors

--- gk2-lab5/waterPS.hlsl
+++ gk2-lab5/waterPS.hlsl
@@ -1,3 +1,4 @@
+textureCUBE envMap;
 float4 camPos;
 sampler samp;
 
@@ -8,13 +9,31 @@ struct PSInput
 	float3 worldPos : POSITION1;
 };
 
+float3 IntersectRay(float3 pos, float3 dir)
+{
+	float3 v_a = max((1 - pos) / dir, (-1 - pos) / dir);
+	float a = min(v_a.x, min(v_a.y, v_a.z));
+
+	return pos + a * dir;
+}
+
 float4 main(PSInput i) : SV_TARGET
 {
 	float3 viewVec = normalize(camPos.xyz - i.worldPos);
 	float3 norm = float3(0.0f, 1.0f, 0.0f);
+	float n = 0.75f;
 
-	float3 color = reflect(-viewVec, norm);
-	//float3 color = refract(-viewVec, norm, 0.75f);
+	if (dot(viewVec, norm) < 0) 
+	{
+		norm = -norm;
+		n = 1 / n;
+	}
 
-	return float4(color, 1.0f);
+	float3 v_reflect = IntersectRay(i.localPos, reflect(-viewVec, norm));
+	float3 reflectColor = envMap.Sample(samp, v_reflect);
+
+	float3 v_refract = IntersectRay(i.localPos, refract(-viewVec, norm, 0.75f));
+	float3 refractColor = envMap.Sample(samp, v_refract);
+
+	return float4(reflectColor, 1.0f);
 }
