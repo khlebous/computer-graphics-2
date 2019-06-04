69d69a405d1c7547068ee75b7a98fdf247eb447a
khlebous <khlebousa@gmail.com>
2019-06-04 17:30:58 +0200

    calculate color using fresnel coefficient

--- gk2-lab5/waterPS.hlsl
+++ gk2-lab5/waterPS.hlsl
@@ -2,6 +2,8 @@ textureCUBE envMap;
 float4 camPos;
 sampler samp;
 
+const float f0 = 0.17f;
+
 struct PSInput
 {
 	float4 pos : SV_POSITION;
@@ -17,11 +19,16 @@ float3 IntersectRay(float3 pos, float3 dir)
 	return pos + a * dir;
 }
 
+float Fresnel(float cos)
+{
+	return f0 + (1 - f0) * pow(1 - cos, 5);
+}
+
 float4 main(PSInput i) : SV_TARGET
 {
 	float3 viewVec = normalize(camPos.xyz - i.worldPos);
 	float3 norm = float3(0.0f, 1.0f, 0.0f);
-	float n = 0.75f;
+	float n = 0.75;
 
 	if (dot(viewVec, norm) < 0) 
 	{
@@ -35,5 +42,8 @@ float4 main(PSInput i) : SV_TARGET
 	float3 v_refract = IntersectRay(i.localPos, refract(-viewVec, norm, 0.75f));
 	float3 refractColor = envMap.Sample(samp, v_refract);
 
-	return float4(reflectColor, 1.0f);
+	float fresnel_coeff = Fresnel(dot(viewVec, norm));
+	float3 color = fresnel_coeff * reflectColor + (1 - fresnel_coeff) * refractColor;
+
+	return float4(pow(color, 0.4545f), 1.0f);
 }
